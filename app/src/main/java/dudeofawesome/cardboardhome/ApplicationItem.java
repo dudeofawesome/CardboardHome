f95cba20b996ddab9cc952e958867c2abb8d603e
Louis Orleans <louis@0rleans.com>
2014-09-06 16:15:38 -0700

    removed unused import

--- app/src/main/java/dudeofawesome/cardboardhome/ApplicationItem.java
+++ app/src/main/java/dudeofawesome/cardboardhome/ApplicationItem.java
@@ -11,7 +11,6 @@ import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 
 public class ApplicationItem {
     public int x = 0;
